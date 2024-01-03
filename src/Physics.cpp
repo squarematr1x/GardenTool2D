27690f54604be40e4bfe48c8e06604b24e37db47
squarematr1x <sasu.pirttipera@protonmail.com>
2024-01-03 20:37:58 +0200

    Implement overlap detection

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -1,20 +1,39 @@
 #include "Physics.h"
-#include "Components.h"
+#include "Entity.h"
 
 namespace Physics
 {
 Vec2 getOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b) {
-	// auto& pos = a->getComponent<CTransform>().pos;
-	(void)a;
-	(void)b;
-	// return the overlap rectangle size of the bounding boxes of entity a and b
-	return Vec2(0, 0);
+	const auto pos_a = a->getComponent<CTransform>().pos;
+	const auto pos_b = b->getComponent<CTransform>().pos;
+	const auto half_size_a = a->getComponent<CBBox>().half_size;
+	const auto half_size_b = b->getComponent<CBBox>().half_size;
+	const auto delta = Vec2(
+		fabsf(pos_a.x - pos_b.x),
+		fabsf(pos_a.y - pos_b.y)
+	);
+
+	// Overlap rectangle size of the bounding boxes of entity a and b
+	const auto overlap_x = half_size_a.x + half_size_b.x - delta.x;
+	const auto overlap_y = half_size_a.y + half_size_b.y - delta.y;
+
+	return Vec2(overlap_x, overlap_y);
 }
 
 Vec2 getPrevOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b) {
-	(void)a;
-	(void)b;
-	// return the previous overlap (uses entity's previous position)
-	return Vec2(0, 0);
+	const auto pos_a = a->getComponent<CTransform>().prevPos;
+	const auto pos_b = b->getComponent<CTransform>().prevPos;
+	const auto half_size_a = a->getComponent<CBBox>().half_size;
+	const auto half_size_b = b->getComponent<CBBox>().half_size;
+	const auto delta = Vec2(
+		fabsf(pos_a.x - pos_b.x),
+		fabsf(pos_a.y - pos_b.y)
+	);
+
+	// Overlap rectangle size of the bounding boxes of entity a and b
+	const auto overlap_x = half_size_a.x + half_size_b.x - delta.x;
+	const auto overlap_y = half_size_a.y + half_size_b.y - delta.y;
+
+	return Vec2(overlap_x, overlap_y);
 }
 } // Physics
