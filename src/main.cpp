33f9ce30f81340591ef934be45563ce61b0ab5e7
squarematr1x <sasu.pirttipera@protonmail.com>
2024-01-08 18:03:22 +0200

    Use relative path in asset files

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "GameEngine.h"
 
 int main() {
-    GameEngine game("/Users/spirttipera/2D-Game-Engine/config/assets.txt");
+    GameEngine game("config/assets.txt");
     game.run();
 
     return 0;
