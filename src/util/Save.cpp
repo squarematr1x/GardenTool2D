aab46f59a5b60b2027688d32c04a3ec19e878c21
squarematr1x <sasu.pirttipera@protonmail.com>
2024-04-17 17:53:41 +0300

    Add skeleton for saving/loading game

---
+++ src/util/Save.cpp
@@ -0,0 +1,9 @@
+#include "Save.h"
+
+void save(const std::string lvl_path) {
+    // Add player config also as a input param
+}
+
+void load(const std::string lvl_path) {
+    // Add player config also as a input param
+}
