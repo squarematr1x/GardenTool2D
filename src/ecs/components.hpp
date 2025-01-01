ce0b67b666684a4893bbb179570c8503073b91bf
squarematr1x <sasu.pirttipera@protonmail.com>
2025-01-01 19:17:48 +0200

    Remove unused components

--- src/ecs/components.hpp
+++ src/ecs/components.hpp
@@ -60,28 +60,6 @@ struct CTransform: Component {
 	}
 };
 
-struct CShape: Component {
-	sf::CircleShape circle;
-
-	CShape(float radius, int points, const sf::Color& fill, const sf::Color& outline, float thickness)
-		: circle(radius, static_cast<std::size_t>(points))
-	{
-		circle.setFillColor(fill);
-		circle.setOutlineColor(outline);
-		circle.setOutlineThickness(thickness);
-		circle.setOrigin(radius, radius);
-	}
-};
-
-struct CCollision: Component {
-	float radius;
-
-	CCollision(float r)
-		: radius(r)
-	{
-	}
-};
-
 struct CScore: Component {
 	int score;
 
